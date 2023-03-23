d2234f037ded0490f795518c8e57f47640cff526
unknown <thomasbarenghi@gmail.com>
2023-03-23 17:12:49 -0300

    pagination style

--- client/src/componentes/general/pagination/pagination.js
+++ client/src/componentes/general/pagination/pagination.js
@@ -1,43 +1,44 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentPage } from "../../../redux/actions/frontend/pagination";
+import styles from "./pagination.module.scss";
+
 
 export default function PaginationComponent({ totalItems, itemsPerPage }) {
 
     const { currentPage, gamesPerPage } = useSelector((state) => state?.frontGames);
     const dispatch = useDispatch();
-  //  console.log('currentPage 1', currentPage);
     const totalPages = Math.ceil(totalItems / itemsPerPage);
     // const pages = [];
     // for (let i = 1; i <= totalPages; i++) {pages.push(i);}
-
     const visiblePageCount = 5;
     const startPage = Math.max(1, currentPage - Math.floor(visiblePageCount / 2));
     const endPage = Math.min(totalPages, startPage + visiblePageCount - 1);
     const pages = [];
 
     for (let i = startPage; i <= endPage; i++) { pages.push(i); }
-    //console.log("Total:", totalPages, "Actual:", currentPage, "Start:", startPage);
 
     useEffect(() => {
-       // console.log('effect');
-        if (currentPage > totalPages) {  dispatch(setCurrentPage(startPage)) };
+        if (currentPage > totalPages) { dispatch(setCurrentPage(startPage)) };
         if (currentPage <= 1) { dispatch(setCurrentPage(1)) };
     }, [totalPages])
 
     return (
-        <div>
-            <button onClick={() => dispatch(setCurrentPage(currentPage - 1))} disabled={currentPage <= 1}>Prev</button>
-            {pages.map((page) => (
-                <button
-                    key={page}
-                    onClick={() => dispatch(setCurrentPage(page))}
-                    disabled={currentPage === page}
-                >
-                    {page}
-                </button>
-            ))}
-            <button onClick={() => dispatch(setCurrentPage(currentPage + 1))} disabled={currentPage >= totalPages}>Next</button>
+        <div className={styles["pagination"]}>
+            <button onClick={() => dispatch(setCurrentPage(currentPage - 1))} disabled={currentPage <= 1} className={currentPage <= 1 ? styles["handlerBtn-disabled"] : styles["handlerBtn"]} >Prev</button>
+            <div className={styles["numberBtns"]}>
+                {pages.map((page) => (
+                    <button
+                        key={page}
+                        onClick={() => dispatch(setCurrentPage(page))}
+                        disabled={currentPage === page}
+                        className={currentPage === page ? styles["numberBtnActive"] : styles["numberBtn"]}
+                    >
+                        {page}
+                    </button>
+                ))}
+            </div>
+            <button onClick={() => dispatch(setCurrentPage(currentPage + 1))} disabled={currentPage >= totalPages} className={currentPage >= totalPages ? styles["handlerBtn-disabled"] : styles["handlerBtn"]} >Next</button>
         </div>
     );
 }
