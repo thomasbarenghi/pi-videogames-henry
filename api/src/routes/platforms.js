a65c26c9654bd2208e57846f5038cf956cbeba7f
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-08-05 19:00:11 -0300

    elimincacion legacy

--- api/src/routes/platforms.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const platformsController = require('../controllers').platformsController;
-router.get('/', platformsController.getPlatforms);
-
-module.exports = router;
