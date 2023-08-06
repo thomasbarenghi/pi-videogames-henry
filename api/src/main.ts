a5ec807d501d39029711f13536143f8ec89f9714
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-08-05 22:33:49 -0300

    merge development

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  ConfigModule.forRoot();
   app.enableCors({
     origin: process.env.URL_FRONT,
   });
